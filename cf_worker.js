31e4a54d8a51094ba782d3280071e80ae256628e
kankankankankankan <vip029@me.com>
2023-04-16 18:41:07 +0800

    增加参数识别及安全过滤

--- cf_worker.js
+++ cf_worker.js
@@ -1,39 +1,45 @@
 addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request))
-  })
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+
+  const { searchParams } = new URL(request.url)
+  let device = searchParams.get('device')
+  let mode = searchParams.get('mode')
   
-  async function handleRequest(request) {
-    
-    const { searchParams } = new URL(request.url)
-    let device = searchParams.get('device')
-    let mode = searchParams.get('mode')
-    console.log(device)
-    if(null===mode||mode!='6'){
-      mode='4'
-    }
-    if(null===device){
-      device='eth0'
-    }
-    let data_url = 'https://raw.githubusercontent.com/RyoLee/routes/gh-pages/eth0/routes' + mode + '.conf'
-    if (request.method !== 'GET') return MethodNotAllowed(request)
-    const response = await fetch(data_url)
-    if (response.ok) {
-      const body = await response.text();
-      return new Response(body.replaceAll('eth0',device))
-    } else {
-      return InternalServerError(request)
-    }
+  if (mode === null && device === null) {
+    return new Response("请在链接中提供网络协议版本(IPv4或IPv6)和网卡名称!", { status: 400 })
   }
-  function MethodNotAllowed(request) {
-    return new Response(`Method ${request.method} not allowed.`, {
-      status: 405,
-      headers: {
-        Allow: 'GET',
-      },
-    });
+  if (mode !== '4' && mode !== '6') {
+    return new Response("网络协议版本参数错误！", { status: 400 })
   }
-  function InternalServerError(request) {
-    return new Response(`Internal Server Error.`, {
-      status: 500,
-    });
+  if (device === null) {
+    device='eth0'
   }
+  
+  let data_url = 'https://raw.githubusercontent.com/RyoLee/routes/gh-pages/eth0/routes' + mode + '.conf'
+  if (request.method !== 'GET') return MethodNotAllowed(request)
+  const response = await fetch(data_url)
+  if (response.ok) {
+    const body = await response.text();
+    return new Response(body.replaceAll('eth0', device))
+  } else {
+    return InternalServerError(request)
+  }
+}
+
+function MethodNotAllowed(request) {
+  return new Response(`请求方法 ${request.method} 不允许。`, {
+    status: 405,
+    headers: {
+      Allow: 'GET',
+    },
+  });
+}
+
+function InternalServerError(request) {
+  return new Response(`内部服务器错误。`, {
+    status: 500,
+  });
+}
